57d6aac796bf6976fda0e71b7066fb3695bad325
intan <2402014@students.poltas.ac.id>
2026-07-02 12:36:19 +0700

    menambahkan pesan pada JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function tampilPesan() {
+    alert("Halo!");
+}
+console.log("Selamat datang di Modul 13");
 const deskripsi = document.getElementById("deskripsi");
 
 const btnUbah = document.getElementById("btnUbah");
